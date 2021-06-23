5f36eab15173524d33c1b45754d1d6aa3a5fe651
Mariusz Zaborski <oshogbo@dynfi.com>
2021-06-23 15:20:58 +0000

    overlay: auto create overlay port

--- prepare_overlay.sh
+++ prepare_overlay.sh
@@ -8,3 +8,5 @@ rm -r dynfi-overlay/dynfi/opnsense-core
 rm -r dynfi-overlay/dynfi/opnsense-update
 cp -iprv freebsd-ports/dynfi/opnsense-core dynfi-overlay/dynfi/opnsense-core
 cp -iprv freebsd-ports/dynfi/opnsense-update dynfi-overlay/dynfi/opnsense-update
+
+sudo -E poudriere ports -c -m null -p dynfi-overlay -M `realpath dynfi-overlay`
