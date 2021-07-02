e14e23f23f65215b63848af2ee840984d466cab4
Mariusz Zaborski <oshogbo@dynfi.com>
2021-07-02 14:16:23 +0000

    bulk: use a local ssh key

--- poudriere-conf/hooks/bulk.sh
+++ poudriere-conf/hooks/bulk.sh
@@ -4,4 +4,4 @@ if [ "$1" != "done" ]; then
     exit 0
 fi
 
-rsync -avz -e "ssh -i /root/pkg-sync-sshkey" --progress /usr/local/poudriere/data/packages/dynfi-13-default/.latest/* pkg@192.168.230.10:/var/www/html/packages/FreeBSD\:13\:amd64
+rsync -avz -e "ssh -i $HOME/.ssh/pkg-sync-sshkey" --progress /usr/local/poudriere/data/packages/dynfi-13-default/.latest/* pkg@192.168.230.10:/var/www/html/packages/FreeBSD\:13\:amd64
