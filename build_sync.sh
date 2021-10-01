b0804157f817637df135351f89ebef8910ba1fa8
Mariusz Zaborski <oshogbo@dynfi.com>
2021-10-01 12:04:46 +0000

    Add variable that describes server for publication.

--- build_sync.sh
+++ build_sync.sh
@@ -5,9 +5,9 @@
 echo ""
 echo "Syncing base"
 echo ""
-rsync -avz -e "ssh -i $HOME/.ssh/pkg-sync-sshkey" --progress ${DYNFI_REPO}/FreeBSD:13:amd64 pkg@192.168.230.10:/var/www/html/base/
+rsync -avz --progress ${DYNFI_REPO}/FreeBSD:13:amd64 ${PUBLIC_SRV}:/var/www/html/base/
 
 echo ""
 echo "Syncing ports"
 echo ""
-rsync -avz -e "ssh -i $HOME/.ssh/pkg-sync-sshkey" --progress poudriere-base/data/packages/${FBSD_BRANCH}-${PORT_BRANCH}/.latest/* pkg@192.168.230.10:/var/www/html/packages/FreeBSD\:13\:amd64
+rsync -avz --progress poudriere-base/data/packages/${FBSD_BRANCH}-${PORT_BRANCH}/.latest/* ${PUBLIC_SRV}:/var/www/html/packages/FreeBSD\:13\:amd64
