16d2a8fa0918f3eb04979c431adba829c3c2c317
Mariusz Zaborski <oshogbo@dynfi.com>
2022-02-26 19:24:51 +0000

    build: Add a devel target and set it as a default

--- build_sync.sh
+++ build_sync.sh
@@ -5,9 +5,9 @@
 echo ""
 echo "Syncing base"
 echo ""
-rsync -avz --progress ${DYNFI_REPO}/FreeBSD:13:amd64 ${PUBLIC_SRV}:/var/www/html/base/
+rsync -avz --progress ${DYNFI_REPO}/FreeBSD:13:amd64 ${PUBLIC_SRV}:/var/www/html/base-dev/
 
 echo ""
 echo "Syncing ports"
 echo ""
-rsync -avz --progress poudriere-base/data/packages/${FBSD_BRANCH}-${PORT_BRANCH}/.latest/* ${PUBLIC_SRV}:/var/www/html/packages/FreeBSD\:13\:amd64
+rsync -avz --progress poudriere-base/data/packages/${FBSD_BRANCH}-${PORT_BRANCH}/.latest/* ${PUBLIC_SRV}:/var/www/html/packages-dev/FreeBSD\:13\:amd64
---
+++ build_sync_prod.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+. common.subr
+
+read -p 'This will change production are you sure? [N/y] ' q
+if [ $q != 'y' ]; then
+	echo "Good decision!"
+	return 0
+fi
+
+echo ""
+echo "Syncing base"
+echo ""
+rsync -avz --progress ${DYNFI_REPO}/FreeBSD:13:amd64 ${PUBLIC_SRV}:/var/www/html/base/
+
+echo ""
+echo "Syncing ports"
+echo ""
+rsync -avz --progress poudriere-base/data/packages/${FBSD_BRANCH}-${PORT_BRANCH}/.latest/* ${PUBLIC_SRV}:/var/www/html/packages/FreeBSD\:13\:amd64
