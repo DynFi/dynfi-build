88b2e3a9a2ae4ebce376b3b52dea00d66d48c701
Mariusz Zaborski <oshogbo@dynfi.com>
2022-11-20 02:38:03 +0000

    build: the DynFi 2.0 will have a sperate branch repo dir

--- build_sync.sh
+++ build_sync.sh
@@ -10,4 +10,4 @@ rsync -avz --progress ${DYNFI_REPO}/FreeBSD:13:amd64 ${PUBLIC_SRV}:/var/www/html
 echo ""
 echo "Syncing ports"
 echo ""
-rsync -avz --progress poudriere-base/data/packages/${FBSD_BRANCH}-${PORT_BRANCH}/.latest/* ${PUBLIC_SRV}:/var/www/html/packages-dev/FreeBSD\:13\:amd64
+rsync -avz --progress poudriere-base/data/packages/${FBSD_BRANCH}-${PORT_BRANCH}/.latest/* ${PUBLIC_SRV}:/var/www/html/packages2-dev/FreeBSD\:13\:amd64
--- build_sync_prod.sh
+++ build_sync_prod.sh
@@ -16,4 +16,4 @@ rsync -avz --progress ${DYNFI_REPO}/FreeBSD:13:amd64 ${PUBLIC_SRV}:/var/www/html
 echo ""
 echo "Syncing ports"
 echo ""
-rsync -avz --progress poudriere-base/data/packages/${FBSD_BRANCH}-${PORT_BRANCH}/.latest/* ${PUBLIC_SRV}:/var/www/html/packages/FreeBSD\:13\:amd64
+rsync -avz --progress poudriere-base/data/packages/${FBSD_BRANCH}-${PORT_BRANCH}/.latest/* ${PUBLIC_SRV}:/var/www/html/packages2/FreeBSD\:13\:amd64
