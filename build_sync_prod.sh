64c23b9939e643e801da80ca9b9a0557a41d229d
Mariusz Zaborski <oshogbo@dynfi.com>
2024-09-08 08:03:03 +0000

    Update production path

--- build_sync_prod.sh
+++ build_sync_prod.sh
@@ -11,9 +11,9 @@ fi
 echo ""
 echo "Syncing base"
 echo ""
-rsync -avz --progress ${DYNFI_REPO}/FreeBSD:13:amd64 ${PUBLIC_SRV}:/var/www/html/base/
+rsync -avz --progress ${DYNFI_REPO}/FreeBSD:14:amd64 ${PUBLIC_SRV}:/var/www/html/base/
 
 echo ""
 echo "Syncing ports"
 echo ""
-rsync -avz --progress poudriere-base/data/packages/${FBSD_BRANCH}-${PORT_BRANCH}/.latest/* ${PUBLIC_SRV}:/var/www/html/packages2/FreeBSD\:13\:amd64
+rsync -avz --progress poudriere-base/data/packages/${FBSD_BRANCH}-${PORT_BRANCH}/.latest/* ${PUBLIC_SRV}:/var/www/html/packages2/FreeBSD\:14\:amd64
